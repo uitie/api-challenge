001bc1665181272b34766120485e4dc1fb2a3f41
Konstantin <konst@ntinh.am>
2021-12-14 15:07:28 -0500

    updated dependendecies and changes to index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
-require('dotenv').config();
+require('dotenv').config({path:__dirname+'../.env'});
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
 const path = require('path');
+const { dirname } = require('path');
+//const { fileURLToPath } = require('url');
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = dirname();
+const PORT = 4000;
 
- 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//stndard route handler
 app.get('/', function (req, res) {
   res.send('Hello World');
+});
+
+
+console.log(PORT);
+//initialize server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
 
 module.exports = app;
